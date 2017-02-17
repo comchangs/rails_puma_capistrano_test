89bb1567d8dc8cb6bd8604c340a315ac2068fa2d
Murry Jeong <comchangs@gmail.com>
2017-02-17 15:18:45 +0900

    #1 Updated capfile and deploy option

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :rbenv_roles, :all # default value
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
